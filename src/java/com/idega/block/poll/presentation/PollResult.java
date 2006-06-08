d20e422f97c828f8604cd5e9644b8bc3f43f3ec2
Sigtryggur Simonarson <sigtryggur@idega.com>
2006-06-08 14:26:26 +0000

    Added localization to closebutton

--- src/java/com/idega/block/poll/presentation/PollResult.java
+++ src/java/com/idega/block/poll/presentation/PollResult.java
@@ -34,7 +34,9 @@ private final static String IW_BUNDLE_IDENTIFIER="com.idega.block.poll";
 protected IWResourceBundle _iwrb;
 protected IWBundle _iwb;
 
+private boolean _isAdmin;
 private boolean _showCollection = false;
+private boolean _save = false;
 private boolean _showVotes = true;
 private int _numberOfPolls = 3;
 private int _pollID = -1;
@@ -55,13 +57,17 @@ public PollResult() {
 }
 
   public void main(IWContext iwc) throws Exception {
-    this._iwrb = getResourceBundle(iwc);
-    this._iwb = getBundle(iwc);
+    _iwrb = getResourceBundle(iwc);
+    _iwb = getBundle(iwc);
 
     setAllMargins(0);
-    setTitle(this._iwrb.getLocalizedString("results","Results"));
+    setTitle(_iwrb.getLocalizedString("results","Results"));
 
-    this._iLocaleID = ICLocaleBusiness.getLocaleId(iwc.getCurrentLocale());
+    /**
+     * @todo permission
+     */
+    _isAdmin = true; //AccessControl.hasEditPermission(this,iwc);
+    _iLocaleID = ICLocaleBusiness.getLocaleId(iwc.getCurrentLocale());
 
     String collectionString = iwc.getParameter(Poll._prmPollCollection);
     String showVotesString = iwc.getParameter(Poll._prmShowVotes);
@@ -69,43 +75,43 @@ public PollResult() {
     String pollIDString = iwc.getParameter(Poll._prmPollID);
 
     try {
-      this._pollQuestionID = Integer.parseInt(iwc.getParameter(PollBusiness._PARAMETER_POLL_QUESTION));
+      _pollQuestionID = Integer.parseInt(iwc.getParameter(PollBusiness._PARAMETER_POLL_QUESTION));
     }
     catch (NumberFormatException e) {
-      this._pollQuestionID = -1;
+      _pollQuestionID = -1;
     }
 
     drawLayout();
 
     if ( collectionString != null ) {
-      this._showCollection = true;
+      _showCollection = true;
     }
     if ( showVotesString != null ) {
       if ( showVotesString.equalsIgnoreCase(PollBusiness._PARAMETER_TRUE) ) {
-        this._showVotes = true;
+        _showVotes = true;
       }
       else {
-        this._showVotes = false;
+        _showVotes = false;
       }
     }
     if ( numberOfPollsString != null ) {
       try {
-        this._numberOfPolls = Integer.parseInt(numberOfPollsString);
+        _numberOfPolls = Integer.parseInt(numberOfPollsString);
       }
       catch (NumberFormatException e) {
-        this._numberOfPolls = 3;
+        _numberOfPolls = 3;
       }
     }
     if ( pollIDString != null ) {
       try {
-        this._pollID = Integer.parseInt(pollIDString);
+        _pollID = Integer.parseInt(pollIDString);
       }
       catch (NumberFormatException e) {
-        this._pollID = -1;
+        _pollID = -1;
       }
     }
 
-    if ( this._showCollection ) {
+    if ( _showCollection ) {
       showCollection(iwc);
     }
     else {
@@ -117,38 +123,37 @@ public PollResult() {
         }
       }
 
-      if ( this._pollQuestionID != -1 ) {
-				this.layoutTable.add(showResults(this._pollQuestionID),1,3);
-			}
+      if ( _pollQuestionID != -1 )
+        layoutTable.add(showResults(_pollQuestionID),1,3);
     }
   }
 
   private void drawLayout() {
-    this.layoutTable = new Table(1,5);
-    this.layoutTable.setWidth(280);
-    this.layoutTable.setCellpadding(0);
-    this.layoutTable.setCellspacing(0);
-    this.layoutTable.setHeight(2,"9");
-    this.layoutTable.setHeight(4,"17");
-    this.layoutTable.setAlignment(1,5,"center");
-    this.layoutTable.setBorder(this.border);
+    layoutTable = new Table(1,5);
+    layoutTable.setWidth(280);
+    layoutTable.setCellpadding(0);
+    layoutTable.setCellspacing(0);
+    layoutTable.setHeight(2,"9");
+    layoutTable.setHeight(4,"17");
+    layoutTable.setAlignment(1,5,"center");
+    layoutTable.setBorder(border);
 
-    Image header = this._iwrb.getImage("top.gif");
-    this.line = this._iwb.getImage("/shared/line.gif");
-    CloseButton close = new CloseButton(this._iwrb.getLocalizedString("close", "Close"));
+    Image header = _iwrb.getImage("top.gif");
+    line = _iwb.getImage("/shared/line.gif");
+    CloseButton close = new CloseButton(_iwrb.getLocalizedString("close", "Close"));
     //CloseButton close = new CloseButton(_iwrb.getImage("close.gif"));
 
-    this.layoutTable.add(header,1,1);
-    this.layoutTable.add(this.line,1,2);
-    this.layoutTable.add(this.line,1,4);
-    this.layoutTable.add(close,1,5);
+    layoutTable.add(header,1,1);
+    layoutTable.add(line,1,2);
+    layoutTable.add(line,1,4);
+    layoutTable.add(close,1,5);
 
-    add(this.layoutTable);
+    add(layoutTable);
   }
 
   private Table showResults(int pollQuestionID) {
     Table myTable = new Table();
-      myTable.setBorder(this.border);
+      myTable.setBorder(border);
       myTable.setWidth(1,1,"2");
       myTable.mergeCells(2,1,6,1);
       myTable.setWidth("100%");
@@ -158,7 +163,7 @@ public PollResult() {
     LocalizedText questionLocText = null;
     try {
       question = ((com.idega.block.poll.data.PollQuestionHome)com.idega.data.IDOLookup.getHomeLegacy(PollQuestion.class)).findByPrimaryKeyLegacy(pollQuestionID);
-      questionLocText = TextFinder.getLocalizedText(question,this._iLocaleID);
+      questionLocText = TextFinder.getLocalizedText(question,_iLocaleID);
     }
     catch (SQLException e) {
       question = null;
@@ -181,14 +186,13 @@ public PollResult() {
             total += answers[i].getHits();
           }
           for (int i = 0 ; i < answers.length ; i++ ) {
-            LocalizedText answerLocText = TextFinder.getLocalizedText(answers[i],this._iLocaleID);
+            LocalizedText answerLocText = TextFinder.getLocalizedText(answers[i],_iLocaleID);
             if ( answerLocText != null ) {
               ++row;
 
               float percent = 0;
-              if ( answers[i].getHits() > 0 ) {
-								percent = ( (float) answers[i].getHits() / (float) total ) * 100;
-							}
+              if ( answers[i].getHits() > 0 )
+                percent = ( (float) answers[i].getHits() / (float) total ) * 100;
 
               Text answerText = new  Text(answerLocText.getHeadline());
                 answerText.setFontSize(1);
@@ -198,9 +202,8 @@ public PollResult() {
                 percentText.setFontSize(1);
 
               myTable.add(answerText,2,row);
-              if ( this._showVotes ) {
-								myTable.add(hitsText,4,row);
-							}
+              if ( _showVotes )
+                myTable.add(hitsText,4,row);
               myTable.add(percentText,6,row);
             }
           }
@@ -225,7 +228,7 @@ public PollResult() {
     int pollSize = 0;
     int first = 0;
     int numberOfAnswers = 0;
-    List pollQuestions = PollBusiness.getPollQuestions(this._pollID);
+    List pollQuestions = PollBusiness.getPollQuestions(_pollID);
     if ( pollQuestions != null ) {
       pollSize = pollQuestions.size();
     }
@@ -249,18 +252,18 @@ public PollResult() {
     }
 
     int row = 1;
-    int tableSize = this._numberOfPolls * 2;
-    if ( pollSize < this._numberOfPolls ) {
+    int tableSize = _numberOfPolls * 2;
+    if ( pollSize < _numberOfPolls ) {
       tableSize = pollSize * 2;
     }
 
     Table myTable = new Table(1,tableSize);
       myTable.setWidth("100%");
-      myTable.setBorder(this.border);
+      myTable.setBorder(border);
 
     if ( pollQuestions != null ) {
       for (int i = first; i < pollSize; i++) {
-        if (i == first + this._numberOfPolls) {
+        if (i == first + _numberOfPolls) {
           break;
         }
 
@@ -272,7 +275,7 @@ public PollResult() {
 
         if (this.line != null) {
           if ( row < tableSize ) {
-            myTable.add(this.line,1,row);
+            myTable.add(line,1,row);
             row++;
           }
         }
@@ -281,28 +284,28 @@ public PollResult() {
 
     setWindowHeight(numberOfAnswers,pollSize);
 
-    Text nextText = new Text(this._iwrb.getLocalizedString("next","Next"));
+    Text nextText = new Text(_iwrb.getLocalizedString("next","Next"));
         nextText.setFontSize(1);
-    Text prevText = new Text(this._iwrb.getLocalizedString("lst","Last"));
+    Text prevText = new Text(_iwrb.getLocalizedString("lst","Last"));
         prevText.setFontSize(1);
 
     Link next = new Link(nextText);
         next.addParameter(Poll._prmPollCollection,PollBusiness._PARAMETER_TRUE);
-        next.addParameter(Poll._prmPollID,Integer.toString(this._pollID));
-        next.addParameter(PollResult.prmPollFirst,Integer.toString(first + this._numberOfPolls));
+        next.addParameter(Poll._prmPollID,Integer.toString(_pollID));
+        next.addParameter(PollResult.prmPollFirst,Integer.toString(first + _numberOfPolls));
     Link prev = new Link(prevText);
         prev.addParameter(Poll._prmPollCollection,PollBusiness._PARAMETER_TRUE);
-        prev.addParameter(Poll._prmPollID,Integer.toString(this._pollID));
-        prev.addParameter(PollResult.prmPollFirst,Integer.toString(first - this._numberOfPolls));
+        prev.addParameter(Poll._prmPollID,Integer.toString(_pollID));
+        prev.addParameter(PollResult.prmPollFirst,Integer.toString(first - _numberOfPolls));
 
 
     Table table = new Table(4,1);
         table.setWidth("100%");
-        table.setBorder(this.border);
+        table.setBorder(border);
         table.setAlignment(3,1,"right");
         table.setWidth(1,1,"2");
         table.setWidth(4,1,"10");
-        if (! (first - this._numberOfPolls < 0)) {
+        if (! (first - _numberOfPolls < 0)) {
           table.add(prev,2,1);
         }
         else {
@@ -310,7 +313,7 @@ public PollResult() {
           table.add(prevText,2,1);
         }
 
-        if ( !( first + this._numberOfPolls >= pollSize ) ) {
+        if ( !( first + _numberOfPolls >= pollSize ) ) {
             table.add(next,3,1);
         }
         else {
@@ -319,7 +322,7 @@ public PollResult() {
         }
 
     myTable.add(table,1,tableSize);
-    this.layoutTable.add(myTable,1,3);
+    layoutTable.add(myTable,1,3);
   }
 
   private void setWindowHeight(int numberOfAnswers) {
